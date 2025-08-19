7e6a871145d5646f3c624b96c71df62de086e780
fatemeh-mkh <fatem.mokhtari@yahoo.com>
2025-08-19 11:20:15 -0700

    feat: add examples for variable declaration (var, let, const) and initialization  var → function-scoped, hoisted, can be re-declared.
    let → block-scoped, not hoisted the same way, can be reassigned.
    
    const → block-scoped, cannot be reassigned.

---
+++ variables.js
@@ -0,0 +1,42 @@
+// تعریف متغیرها
+let name = "Ehsan";
+const age = 22;
+var city = "Tehran";
+var myname= "Fatemeh";
+let ourName = "freecodeCamp";
+const pi = 3.14;
+myOld = 8;
+var country = "Iran"; 
+
+
+// Reassigning values
+let color = "red";
+color = "blue"; 
+console.log("Color is:", color);
+
+let temp;
+console.log("Temp:", temp); // undefined
+
+
+// Initializing variables
+var a;
+console.log(a);
+
+var b=10; 
+a = b ;
+
+console.log(b)
+
+// Uninitialized variables
+
+var m = 10;
+var l = 5;
+var c = "Hello ";
+
+m = m + 2;
+l = l - 3;
+c = c + "From Here!";
+
+console.log(m);
+console.log(l);
+console.log(c);
