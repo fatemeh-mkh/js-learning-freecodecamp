d68cbfb584f547cc882ae743ca32367ab45855d8
fatemeh-mkh <fatem.mokhtari@yahoo.com>
2025-08-19 11:36:08 -0700

    add examples for JavaScript case sensitivity

---
+++ caseSensitive.js
@@ -0,0 +1,31 @@
+
+
+var studlyCapVar;
+
+var properCamelCase;
+
+var titleCaseOver;
+
+STUDLYCAPVAR = 10;
+
+PROPERCAmeLCase ="Hi";
+
+titileCaseoVER = 50;
+
+console.log(studlyCapVar);
+
+
+// Variable case sensitivity
+let myName = "Fatemeh";
+let myname = "Ehsan";
+
+console.log(myName);  // Fatemeh
+console.log(myname);  // Ehsan
+
+
+// String comparison is case sensitive
+let str1 = "JavaScript";
+let str2 = "javascript";
+
+console.log(str1 === str2); // false
+console.log(str1.toLowerCase() === str2.toLowerCase()); // true
