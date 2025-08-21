2be39cb791bb1023db605336414696ba4f81a02f
fatemeh-mkh <fatem.mokhtari@yahoo.com>
2025-08-21 07:01:57 -0700

    Implement queue behavior with array push and shift

--- Functions.js
+++ Functions.js
@@ -82,3 +82,14 @@ function addThree(){
 console.log(addThree());
 
 
+// Stand in Line
+function nextInLine(arr , item){
+ arr.push(item);
+ return arr.shift();
+}
+
+var testArr = [1,2,3,4,5,6]
+
+console.log("Before: "+ JSON.stringify(testArr));
+console.log(nextInLine(testArr,7));
+console.log("After: " + JSON.stringify(testArr));
