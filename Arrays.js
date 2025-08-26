53e01d9e60b05e69d9a881b3a10e0e377f58b771
fatemeh-mkh <fatem.mokhtari@yahoo.com>
2025-08-26 11:12:27 -0700

    Add examples of object destructuring assignment

--- Arrays.js
+++ Arrays.js
@@ -105,3 +105,23 @@ const updatedUser = { ...user, city: "Tehran" };
 console.log(updatedUser); 
 // { name: "Ali", age: 25, city: "Tehran" }
 
+var voxel = {x: 3.6 ,y: 7.4, z:6.54};
+
+var x =voxel.x;
+var y =voxel.y;
+var z =voxel.z;
+
+const {x : a, y: b , z : c} = voxel;
+
+const AVG_TEMPERATURES = {
+  today : 77.5,
+  tomorrow : 79
+};
+
+function getTempOfTmrw(avgTemperatures){
+  "use strict";
+  const {tomorrow : tempOgTomorrow}  = avgTemperatures;
+  return tempOgTomorrow;
+}
+
+console.log(getTempOfTmrw(AVG_TEMPERATURES));
