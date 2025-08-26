bbcdc96c2dbb1c91bb025eeaffa6736824fbf5a9
fatemeh-mkh <fatem.mokhtari@yahoo.com>
2025-08-26 08:19:24 -0700

    Implemented sum with (...args) to accept any number of arguments

--- Functions.js
+++ Functions.js
@@ -237,3 +237,24 @@ const increment = (
 
 console.log(increment(2,5));
 console.log(increment(5));
+
+// use the rest operator with function parameter
+const sum1 = (function(){
+return function sum1(x, y, z){
+const args = [x,y,z];
+return args.reduce((a,b) => a+b , 0);
+};
+})();
+
+console.log(sum1(1,2,3));
+
+
+// instead better method
+const sum2 = (...args) => {
+  return args.reduce((a, b) => a + b, 0);
+};
+console.log(sum2(1, 2, 3));          // → 6
+console.log(sum2(5, 10, 15, 20));    // → 50
+console.log(sum2());                 // → 0 (بدون ورودی)
+console.log(sum2(100)); 
+
